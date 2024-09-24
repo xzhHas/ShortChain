6eaefe8feb9b7b5dcf0e6cbf3b1f705b8d2eccc6
xiwanli <2597029604@qq.com>
2024-09-24 20:37:24 +0800

    v1,mysqlID和本地Base62

--- internal/interfaces/v1.go
+++ internal/interfaces/v1.go
@@ -11,10 +11,13 @@ import (
 )
 
 func (h *Handler) RedirectToLongUrlV1(ctx *gin.Context) {
-
-	fullUrl := ctx.Request.RequestURI // 获取完整的请求URL
+	// URL：是完整的地址，包括协议和主机信息
+	// URI：关注请求的路径和查询的部分
+	fullUrl := ctx.Request.RequestURI // 获取完整的请求URL，也就是获取路由的路径
+	//fmt.Printf("fullurl：%s\n", fullUrl)
 	urlParts := strings.Split(fullUrl, "/v1/")
-
+	///shorturlx/v1/y
+	//fmt.Println(urlParts) /shorturlx y
 	if len(urlParts) != 2 || urlParts[1] == "" {
 		log.Infof("getLongUrl: 无效的短链 %s", fullUrl)
 		ctx.Status(http.StatusNotFound)
@@ -22,6 +25,7 @@ func (h *Handler) RedirectToLongUrlV1(ctx *gin.Context) {
 	}
 
 	shortURL := urlParts[1]
+	//fmt.Println(shortURL)  y
 	reply, err := h.shortUrlXService.GetLongUrlV1(context.Background(), &pb.GetLongUrlRequest{ShortUrl: shortURL})
 	if err != nil {
 		log.Errorf("controller.RedirectToLongUrlV1 err: %v", err)
@@ -36,13 +40,15 @@ func (h *Handler) RedirectToLongUrlV1(ctx *gin.Context) {
 		return
 	}
 
-	// 存在，重定向到对应短链
+	// 存在，重定向到对应短链，301
 	ctx.Redirect(http.StatusMovedPermanently, reply.LongUrl)
 }
 
 func (h *Handler) CreateShortUrlV1(ctx *gin.Context) {
+	// kratos 框架集成 gin框架用法，使用gin.Context
+	// 这里使用微服务，使用protobuf，有用于grpc通信的Request和回复Reply
 	var req pb.CreateShortUrlRequest
-
+	// 将客户端发送的json数据，ShouldBind给req
 	if err := ctx.ShouldBind(&req); err != nil {
 		log.Errorf("CreateShortUrlV1 err: %v", err.Error())
 		ctx.JSON(200, gin.H{
@@ -50,7 +56,7 @@ func (h *Handler) CreateShortUrlV1(ctx *gin.Context) {
 		})
 		return
 	}
-
+	// 通过grpc调用后端服务
 	reply, err := h.shortUrlXService.GenerateShortUrlV1(context.Background(), &req)
 	if err != nil {
 		log.Errorf("CreateShortUrlV1 err: %v", err)
@@ -60,7 +66,5 @@ func (h *Handler) CreateShortUrlV1(ctx *gin.Context) {
 		return
 	}
 
-	ctx.JSON(200, pb.CreateShortUrlReply{
-		ShortUrl: fmt.Sprintf("http://%s/v1/%s", ctx.Request.Host, reply.ShortUrl)})
-
+	ctx.JSON(200, pb.CreateShortUrlReply{ShortUrl: fmt.Sprintf("http://%s/v1/%s", ctx.Request.Host, reply.ShortUrl)})
 }
--- internal/service/v1.go
+++ internal/service/v1.go
@@ -5,18 +5,24 @@ import (
 	pb "github.com/BitofferHub/proto_center/api/shortUrlXsvr/v1"
 )
 
-func (s *ShortUrlXService) GenerateShortUrlV1(ctx context.Context, req *pb.CreateShortUrlRequest) (*pb.CreateShortUrlReply, error) {
-	shortUrl, err := s.uc.GenerateShortUrlV1(ctx, req.LongUrl)
-	if err != nil {
-		return nil, err
-	}
-
-	return &pb.CreateShortUrlReply{ShortUrl: shortUrl}, nil
-}
+// GetLongUrlV1 调用业务逻辑层，获取长链
+// @return LongUrl
 func (s *ShortUrlXService) GetLongUrlV1(ctx context.Context, req *pb.GetLongUrlRequest) (*pb.GetLongUrlReply, error) {
 	longUrl, err := s.uc.GetLongUrlV1(ctx, req.ShortUrl)
 	if err != nil {
 		return nil, err
 	}
 	return &pb.GetLongUrlReply{LongUrl: longUrl}, nil
+}
+
+// GenerateShortUrlV1 调用业务逻辑层，生成或更新短链
+// @return ShortUrl
+func (s *ShortUrlXService) GenerateShortUrlV1(ctx context.Context, req *pb.CreateShortUrlRequest) (*pb.CreateShortUrlReply, error) {
+	// 应用层调用，主要负责协调输入请求和底层逻辑的处理
+	shortUrl, err := s.uc.GenerateShortUrlV1(ctx, req.LongUrl)
+	if err != nil {
+		return nil, err
+	}
+
+	return &pb.CreateShortUrlReply{ShortUrl: shortUrl}, nil
 }
